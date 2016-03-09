1bc73e1fd1075f6ad8adcc7a623c3269bac5138d
Ansh <lightnarcissus@gmail.com>
2016-03-09 14:08:15 -0500

    Created login/register page and a simple web request script

---
+++ BarterFriend/Assets/LoginManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class LoginManager : MonoBehaviour {
+
+	public GameObject loginPage;
+	public GameObject registerPage; 
+
+	//login page
+	public Text loginUsername;
+	public Text loginPassword;
+
+	//register page
+	public Text registerEmail;
+	public Text registerUsername;
+	public Text registerPassword;
+
+	private int loginState = 0;
+
+	// Use this for initialization
+	void Start () {
+		loginState = 1;
+		registerPage.SetActive (false);
+		loginPage.SetActive (true);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoginUser()
+	{
+		
+	}
+
+	public void MakeUserRegister()
+	{
+		loginState = 2;
+		loginPage.SetActive (false);
+		registerPage.SetActive (true);
+	}
+	public void RegisterUser()
+	{
+		//registers
+		loginPage.SetActive (false);
+		registerPage.SetActive (true);
+	}
+}
---
+++ BarterFriend/Assets/StateManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateManager : MonoBehaviour {
+
+	public int appState=0;
+	// Use this for initialization
+	void Awake()
+	{
+		DontDestroyOnLoad (this.gameObject);
+		appState = 1;
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		switch (appState) {
+
+		case 1:
+			if (Input.GetMouseButtonDown (0)) {
+				appState = 2;
+				Application.LoadLevel ("LoginPage");
+			}
+			break;
+		case 2:
+			break;
+		}
+	
+	}
+}
