e651f0a5852f86c074579ca01e7aaf694bdb50ef
Ansh <lightnarcissus@gmail.com>
2016-03-10 11:55:12 -0500

    Another set of changes on PC

---
+++ BarterFriend/Assets/RequestPageManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class RequestPageManager : MonoBehaviour {
+
+	//request properties
+	public InputField reqTitle;
+	public Dropdown reqType;
+	public InputField reqDescription;
+	public Toggle person;
+	public Toggle video;
+	public Toggle call;
+	public Toggle text;
+	public Toggle friendsOnly;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SubmitRequest()
+	{
+	}
+}
